f5cf753f5a7adb33d4743b54fa846177c5595409
JasonGiak <iasongiakoumelos@gmail.com>
2024-03-02 22:09:32 +0200

    pare to arxeio bro

--- test.c
+++ test.c
@@ -1,8 +1,33 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(){
+int sum(int lab, int ergasia, int exetasi){
+    int telikos;
+    telikos = lab * 0.3 + ergasia * 0.2 + exetasi * 0.5;
 
-    printf("Hello Bitch");
+    return telikos;
+}
+
+
+
+int main(int argc, char **argv){
+
+if(argc != 4){
+    printf("eisai malakas vale 3");
+    return 0;
+}
+
+int x = atoi(argv[1]);
+int y = atoi(argv[2]);
+int z = atoi(argv[3]);
+
+
+
+
+
+printf("%d",sum(x,y,z));
+
 
 return 0;
 }
+
